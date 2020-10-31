4e042cb4e12b24e183880885c491f12877af8971
Thomas George <43654548+thethomaswgeorge@users.noreply.github.com>
2020-10-31 17:24:48 -0500

    Update 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+import { environment } from '../environments/environment';
 @NgModule({
     declarations: [AppComponent],
     entryComponents: [],
@@ -15,6 +22,10 @@ import { AppComponent } from './app.component';
         BrowserModule,
         IonicModule.forRoot(),
         AppRoutingModule,
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        AngularFireAuthModule,
+        AngularFireDatabaseModule,
+        AngularFireStorageModule
     ],
     providers: [
         StatusBar,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    firebaseConfig: {
+        apiKey: 'AIzaSyCPGsdpfu2tZh7eIAmHiDTIznYY5_LxQ7A',
+        authDomain: 'contact-app-418a6.firebaseapp.com',
+        databaseURL: 'https://contact-app-418a6.firebaseio.com',
+        projectId: 'contact-app-418a6',
+        storageBucket: 'contact-app-418a6.appspot.com',
+        messagingSenderId: '915464095229',
+        appId: '1:915464095229:web:54be2863cffe822f188594',
+        measurementId: 'G-R44SYN5WNC',
+    },
 };
 
 /*
