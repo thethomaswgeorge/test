5385a4d84b224d8046a3c6fd9443f45c22e2d5f2
Thomas George <43654548+thethomaswgeorge@users.noreply.github.com>
2020-11-07 16:23:30 -0600

    Fourth Push for today

--- src/app/tab2/create-event/create-event.component.ts
+++ src/app/tab2/create-event/create-event.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-create-event',
-  templateUrl: './create-event.component.html',
-  styleUrls: ['./create-event.component.scss'],
+    selector: 'app-create-event',
+    templateUrl: './create-event.component.html',
+    styleUrls: ['./create-event.component.scss'],
 })
 export class CreateEventComponent implements OnInit {
+    form = new FormGroup({
+        name: new FormControl('', [Validators.required]),
+    });
 
-  constructor(
-      public modalCtrl: ModalController,
-  ) { }
+    constructor(
+        public modalCtrl: ModalController,
+    ) {
+    }
 
-  ngOnInit() {}
+    ngOnInit() {
+    }
 
     dismiss() {
         this.modalCtrl.dismiss();
+    }
+
+    submit() {
+        alert('This part isn\'t completed yet');
     }
 }
